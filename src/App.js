6ad080669c750ff9e21b6f8ab9b74462cf57be70
Rui Abreu <rui.abreu@impactzero.pt>
2021-03-08 15:02:00 +0000

    Styling the Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import "./App.css";
 
-import './App.css';
-
-import Header from './Components/Header/Header';
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
       {/* Header Component */}
-      <Header/>
+      <Header />
       {/* Home Component */}
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-//import './Header.css';
+import "./Header.css";
 
 function Header() {
   return (
-    <div className="'header">
+    <div className="header">
       <img
-        className="header__logo"
+        className="header_logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt=""
       ></img>
@@ -15,9 +15,18 @@ function Header() {
       </div>
 
       <div className="header_nav">
-        <div className="header_option"></div>
-        <div className="header_option"></div>
-        <div className="header_option"></div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineOne">Sign In</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineOne">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineOne">Prime</span>
+        </div>
       </div>
     </div>
   );
