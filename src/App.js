282145c24bd6fe5aea56ccda00e694396ee974c3
Rui Abreu <rui.abreu@impactzero.pt>
2021-03-15 12:06:36 +0000

    Add router to project for starting build the checkout page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import "./App.css";
 
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
    return (
-      <div className="app">
-         {/* Header Component */}
-         <Header />
-         {/* Home Component */}
-         <Home />
-      </div>
+      <Router>
+         <div className="app">
+            <Switch>
+               <Route path="/checkout">
+                  {/* Header Component */}
+                  <Header />
+                  {/* Checkout Component */}
+                  <h1>I AM CHECKOUT, SMASH THE LIKE BUTTON</h1>
+               </Route>
+               <Route path="/">
+                  {/* Header Component */}
+                  <Header />
+                  {/* Home Component */}
+                  <Home />
+               </Route>
+            </Switch>
+         </div>
+      </Router>
    );
 }
 
